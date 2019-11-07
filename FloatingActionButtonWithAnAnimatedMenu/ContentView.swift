7e34dbeaabc95e69bfbddcc10fe6f17875f6a57d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-07 17:24:14 +0200

    init

--- FloatingActionButtonWithAnAnimatedMenu/ContentView.swift
+++ FloatingActionButtonWithAnAnimatedMenu/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showMenuItem1 = false
+    @State private var showMenuItem2 = false
+    @State private var showMenuItem3 = false
+    
     var body: some View {
-        Text("Hello World")
+        
+        ZStack(alignment: .bottomTrailing) {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            
+            VStack {
+                Spacer()
+                if showMenuItem1 {
+                    MenuItem(icon: "camera.fill")
+                }
+                if showMenuItem2 {
+                    MenuItem(icon: "photo.on.rectangle")
+                }
+                if showMenuItem3 {
+                    MenuItem(icon: "square.and.arrow.up.fill")
+                }
+                
+                Button(action: {
+                    self.showMenu()
+                    print("Show floating menu")
+                }) {
+                    Image(systemName: "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .foregroundColor(Color(red: 153/255, green: 102/255, blue: 255/255))
+                        .shadow(color: .gray, radius: 0.3, x: 1, y: 1)
+                }.padding()
+            }
+        }
+    }
+    
+    func showMenu() {
+        withAnimation {
+            self.showMenuItem3.toggle()
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            withAnimation {
+                self.showMenuItem2.toggle()
+            }
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            withAnimation {
+                self.showMenuItem1.toggle()
+            }
+        }
     }
 }
 
@@ -19,3 +69,22 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct MenuItem: View {
+    
+    var icon: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(Color(red: 153/255, green: 102/255, blue: 255/255))
+                .frame(width: 55, height: 55)
+            Image(systemName: icon)
+                .imageScale(.large)
+                .foregroundColor(.white)
+        }.shadow(color: .gray, radius: 0.3, x: 1, y: 1)
+            .transition(.move(edge: .trailing))
+    }
+}
+
+
